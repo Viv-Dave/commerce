3adb7fc553db0e85a65e9485f5a9c492a6d7904b
Vivek <vivdave2004@gmail.com>
2024-11-05 17:31:49 +0530

    made majority migrations

--- auctions/models.py
+++ auctions/models.py
@@ -1,22 +1,33 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-
+from django.core.exceptions import ValidationError
 class User(AbstractUser):
     user_name = models.BigAutoField(primary_key=True)
+
     def __str__(self):
-        return f"{self.first_name} {self.last_name} ({self.user_name})"
+        return f"{self.username}"
 
 class Listing(models.Model):
     listing_id = models.BigAutoField(primary_key=True)
     auction_item = models.CharField(max_length=64)
-    start_date = models.DateField()
-    end_date = models.DateField()
     price = models.IntegerField()
     updated_price = models.IntegerField()
-    category = models.CharField(max_length=64)
+    category = models.CharField(max_length=64, default="General")
+    active_status = models.BooleanField(default=True)
     def __str__(self):
-        return f"ID: {self.listing_id} Item: {self.auction_item} Category: {Listing.category}"
+        return f"ID: {self.listing_id} Item: {self.auction_item} Category: {self.category}"
+class Bid(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    username = models.ForeignKey(User, on_delete=models.CASCADE, related_name="bidder")
+    product_id = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name="commentator")
+    bid_price = models.IntegerField()
 
+    def is_active(self):
+        if not self.product_id.active_status:
+            raise ValidationError("Can only place a bid if the listing is active.")
+        return True 
+    def __str__(self):
+        return f"{self.username} places bid of {self.bid_price} on product {self.product_id}"
 class Comment(models.Model):
     id = models.BigAutoField(primary_key=True)
     username = models.ForeignKey(User, on_delete=models.CASCADE, related_name="commentator")
